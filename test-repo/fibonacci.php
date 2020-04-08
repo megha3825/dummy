dd5f09a2bf52fff12d2ac304ec03611935e922b7
megha3825 <60346890+megha3825@users.noreply.github.com>
2020-04-08 19:32:27 +0530

    Add files via upload

---
+++ test-repo/fibonacci.php
@@ -0,0 +1,36 @@
+
+
+<!DOCTYPE html>
+<head>
+	<title> fibonacci series</title>
+</head>
+<body>
+	
+	<form action="" method="post">
+		<input type="number" name="input"><br>
+		<input type="submit" name="sub"><br>
+	</form>
+</body>
+</html>
+
+<?php
+
+$a=0;
+$b=1;
+	
+	if(!empty($_POST['sub']))
+{
+		$input=$_POST['input'];
+
+	while($input>0)
+	{
+		echo $a . ",";
+		$c= $a+$b;
+		$a=$b;
+		$b=$c;
+		$input=$input-1;
+	}
+} 
+
+
+?>
